51f2848f52e872e68bbb0d1395b2093cdd0e0769
daniel <danielreubenkelly@gmail.com>
2025-07-30 00:22:13 +0100

    comment to explain unused config structs

--- config/config.go
+++ config/config.go
@@ -1,29 +1,31 @@
 package config
 
+//some placeholder structs to show how this would work
 type JamfProConfig struct {
 	JamfURL string
 }
 type ZenDeskConfig struct {
 	ZenDeskURL string
-	userlist   map[string]int
 }
 type ActiveDirectoryConfig struct {
 	LDAPURL    string
 	Domain     string
 	SearchRoot string
 }
+
 //	namefix structs start
-type NamefixConfig struct{
+type NamefixConfig struct {
 	Replacements []Replacement `json:"Replacements"`
 }
 type Replacement struct {
 	Match       string `json:"Match"`
 	Replacement string `json:"Replacement"`
 }
+
 //	namefix structs end
 type MasterConfig struct {
-	JamfConf JamfProConfig
-	ZenConf ZenDeskConfig
-	ADConf   ActiveDirectoryConfig
+	JamfConf    JamfProConfig
+	ZenConf     ZenDeskConfig
+	ADConf      ActiveDirectoryConfig
 	NamefixConf NamefixConfig
 }
